8d4bda5537532ee3c70204404b40b8812c961c39
NishantTheProgrammer <knishant113@gmail.com>
2020-09-03 15:32:58 +0530

    Menu bar UI done

---
+++ script.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            hamburger: false,
+            dark: false
+        }
+    },
+    methods: {
+        
+    },
+});
